fe0208a2eca4354afbb00985f9859f79db759949
Tobias Lang <tobias@motesque.com>
2020-01-03 20:38:12 -0500

    wip

--- automation/jenkins_artifact.sh
+++ automation/jenkins_artifact.sh
@@ -7,3 +7,4 @@ mkdir -p /var/docker-images/
 # use the last entry as the TAG
 DOCKER_TAG=$(grep FROM containers/$THIS_CONTAINER/Dockerfile | tail -n 1 | cut -d : -f 2)-git$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION  | cut -c1-7)
 docker save motesque/codebuild-$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG} | gzip > /var/docker-images/motesque-codebuild-$THIS_CONTAINER-$THIS_ARCH-debian-${DOCKER_TAG}.tar.gz
+ls -al /var/docker-images/
