ec42e0108d4b09395358c5f4c07c8da029b66fc8
Tobias Lang <tobias@motesque.com>
2020-01-05 16:22:06 -0500

    wip

--- automation/jenkins_publish.sh
+++ automation/jenkins_publish.sh
@@ -2,6 +2,7 @@
 THIS_ARCH=$1
 THIS_CONTAINER=$2
 
+docker login
 # use the last entry as the TAG
 DOCKER_TAG=$(grep FROM containers/$THIS_CONTAINER/Dockerfile | tail -n 1 | cut -d : -f 2)-git$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION  | cut -c1-7)
 echo "Pushing Docker Base Image for '$THIS_CONTAINER' - '$THIS_ARCH'"
