fb547cd395a3c9227e407f962d7540598bf223a6
Daniel Galvao <dgalvao@gmail.com>
2020-12-01 10:38:51 +0100

    Adding push to ECR

--- automation/jenkins_publish.sh
+++ automation/jenkins_publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 THIS_ARCH=$1
 THIS_CONTAINER=$2
+export REPO_BASE=473689091316.dkr.ecr.eu-central-1.amazonaws.com/base-images
 
 # use the last entry as the TAG
 DOCKER_TAG=$(grep FROM containers/$THIS_CONTAINER/Dockerfile | tail -n 1 | cut -d : -f 2)-git$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION  | cut -c1-7)
@@ -9,3 +10,8 @@ docker push motesque/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG}
 # push again with latest tag
 docker tag motesque/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG} motesque/$THIS_CONTAINER-$THIS_ARCH-debian:latest
 docker push motesque/$THIS_CONTAINER-$THIS_ARCH-debian:latest
+# push to ECR:
+docker tag motesque/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG} ${REPO_BASE}/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG}
+docker tag motesque/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG} ${REPO_BASE}/$THIS_CONTAINER-$THIS_ARCH-debian:latest
+docker push ${REPO_BASE}/$THIS_CONTAINER-$THIS_ARCH-debian:${DOCKER_TAG}
+docker push ${REPO_BASE}/$THIS_CONTAINER-$THIS_ARCH-debian:latest
